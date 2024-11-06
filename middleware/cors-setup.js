99a68437403c72eb9c77b2cd446eccaf24b91640
berlinlee-phoenix <berlinlee2017@gmail.com>
2024-11-06 20:17:07 +0800

    cors

--- middleware/cors-setup.js
+++ middleware/cors-setup.js
@@ -1,6 +1,6 @@
 module.exports = (error, req, res, next) => {
   // res.setHeader('Access-Control-Allow-Origin', '*');
-  const isProduction = process.env.NODE_ENV === 'production' ? `https://little-mern-frontend.com` : '*';
+  const isProduction = process.env.NODE_ENV === 'production' ? `https://little-mern-react.onrender.com` : '*';
   
   res.setHeader('Access-Control-Allow-Origin', isProduction);
   res.setHeader(
